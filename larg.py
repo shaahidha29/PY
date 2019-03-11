b9e8892e26949eb31c32b457e6e448676f5243e7
shaahidha29 <47552967+shaahidha29@users.noreply.github.com>
2019-03-11 20:19:45 +0530

    larg.py

---
+++ larg.py
@@ -0,0 +1,9 @@
+num1=12
+num2=35
+num3=8
+if(num1>=num2) and (num1>=num3):
+  print("num1 is largest")
+elif(num2>=num3) and (num2>=num1):
+  print("num2 is largest")
+else:
+  print("num3 is largest")
